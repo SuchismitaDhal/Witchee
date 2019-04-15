f12b626eb977e12213c8a4143082126d5f94d993
Suchismita Dhal <34514700+SuchismitaDhal@users.noreply.github.com>
2019-04-15 21:35:56 +0530

    add the cover page

---
+++ animatefront.js
@@ -0,0 +1,48 @@
+/********
+	function tracks mouse movement for custom cursor
+********/
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+ (e.pageY-10) +"px; left: "+ (e.pageX-10) +"px;")
+});
+
+
+
+/********
+	event listeners when cursor enters and exits the logo
+********/
+
+var logo = document.querySelector('.logo');
+
+logo.addEventListener('mouseover', function(){
+    //console.log("Mouse hovered");
+    cursor.classList.add('cursor-hit');
+});
+
+logo.addEventListener('mouseleave', function(){
+    cursor.classList.remove('cursor-hit');
+    //console.log("transition done");
+});
+
+
+/********
+	swipe functionality on spacebar
+********/
+var item = document.querySelector('.footer h1:nth-child(2)');
+document.body.onkeydown = function(e){
+    if(e.keyCode == 32)
+    {
+        item.setAttribute("style", "-webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%); clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);");
+    }
+    
+}
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32)
+    {
+    	item.setAttribute("style", "-webkit-clip-path: polygon(0 0, 0 0, 0 100%, 0% 100%); clip-path: polygon(0 0, 0 0, 0 100%, 0% 100%);");
+     
+    }
+
+}
